ee4589b8adbfb57c629eaeec57728de6111b49af
Robert Snyder <r.snyder49@gmail.com>
2019-01-03 20:20:03 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,10 @@
 class RepositoriesController < ApplicationController
   
   def index
+    
+    user = Faraday.get 'https://api.github.com/user' do |req|
+      req.headers['Authorization'] = 'token ' + session['token']
+      req.headers['Accept'] = 'application/json'
   end
 
 end
